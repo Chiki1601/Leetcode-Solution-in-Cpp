63a174bd9c0ed5a91255b54677e3a6da8c380b2d
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-07-23 11:31:08 +0530

    Create 232.cpp

---
+++ 232.cpp
@@ -0,0 +1,29 @@
+//232. Leetcode Solution in Cpp
+class MyQueue {
+ public:
+  void push(int x) {
+    input.push(x);
+  }
+
+  int pop() {
+    peek();
+    const int val = output.top();
+    output.pop();
+    return val;
+  }
+
+  int peek() {
+    if (output.empty())
+      while (!input.empty())
+        output.push(input.top()), input.pop();
+    return output.top();
+  }
+
+  bool empty() {
+    return input.empty() && output.empty();
+  }
+
+ private:
+  stack<int> input;
+  stack<int> output;
+};
