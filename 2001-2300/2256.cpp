9634ee64963985202339007f576ec789104f92b0
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-07-25 20:43:47 +0530

    Create 2256.cpp

---
+++ 2001-2300/2256.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minimumAverageDifference(vector<int>& nums) {
+         long long total_sum = 0;
+         for(int  ele : nums) {
+             total_sum = total_sum + (long long) (ele);
+         }
+         
+         long long ans = LLONG_MAX;
+         long long j = 0;
+         long long cur_sum = 0;
+         for(int i = 0; i < nums.size(); i++){
+             cur_sum = cur_sum + (long long) nums[i];
+             long long left = 0, right = 0;
+              left = (cur_sum) / (i + 1);
+             if(i < nums.size() - 1)
+             right = (total_sum - cur_sum) / (nums.size() - i - 1);
+             int diff = abs(left - right);
+             if(diff < ans) {
+                 ans = diff;
+                 j = i;
+             }
+         }
+        return j;
+         
+    }
+};
