36040bfa671e6b2ebf6ead8b647ef23269c1536e
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-16 12:09:33 +0530

    Create 2309.cpp

---
+++ 2301-2400/2309.cpp
@@ -0,0 +1,16 @@
+class Solution {
+ public:
+  string greatestLetter(string s) {
+    vector<bool> seen(128);
+
+    for (const char c : s)
+      seen[c] = true;
+
+    for (int i = 25; i >= 0; --i)
+      if (seen['a' + i] && seen['A' + i])
+        return string(1, 'A' + i);
+
+    return "";
+  }
+};
+c
