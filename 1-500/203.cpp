e74186d49b5406ffe97bf8cd176ed181e543d365
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-26 12:53:27 +0530

    Create 203.cpp

---
+++ 1-500/203.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+ public:
+  ListNode* removeElements(ListNode* head, int val) {
+    ListNode dummy(0, head);
+    ListNode* prev = &dummy;
+
+    for (; head; head = head->next)
+      if (head->val != val) {
+        prev->next = head;
+        prev = prev->next;
+      }
+    prev->next = nullptr;  // in case the last val == val
+
+    return dummy.next;
+  }
+};
