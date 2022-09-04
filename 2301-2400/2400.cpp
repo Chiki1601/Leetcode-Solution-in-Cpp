accb0b0f73a9f0d4a09663ab137cbf2fdd0b010f
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-09-04 10:54:27 +0530

    Create 2400.cpp

---
+++ 2301-2400/2400.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+int mod = 1e9 + 7;
+long dp[3100][1001];
+
+long helper(int start , int end , int k){
+    if(start==end){
+       if(k==0) return 1;
+    }
+    if(k==0) return 0;
+
+    if(dp[start+1000][k]!=-1) return dp[start+1000][k];
+
+
+    long ans = helper(start+1,end,k-1);
+
+  
+    if((k-1) >= (end-start+1)){
+        ans += helper(start-1,end,k-1);
+    }
+    return dp[start+1000][k]=ans%mod;
+}
+int numberOfWays(int sp, int ep, int k) {
+    memset(dp,-1,sizeof(dp));
+    if(abs(sp-ep)>k) return 0;
+    
+    // helper
+    long ans = helper(sp,ep,k);
+    return ans%mod;
+}
+};
