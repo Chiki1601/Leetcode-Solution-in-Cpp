964891d0d1fccc7cad59bf17524e2c5f178d9dfe
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-09-12 23:16:15 +0530

    Create 2406.cpp

---
+++ 2401-2500/2406.cpp
@@ -0,0 +1,30 @@
+class Solution {
+
+ public:
+
+  // very similar to 253. Meeting Rooms II
+
+  int minGroups(vector<vector<int>>& intervals) {
+
+    // store `right`s
+
+    priority_queue<int, vector<int>, greater<>> minHeap;
+
+    sort(begin(intervals), end(intervals));
+
+    for (const auto& interval : intervals) {
+
+      if (!minHeap.empty() && interval[0] > minHeap.top())
+
+        minHeap.pop();  // no overlap, we can reuse the same group
+
+      minHeap.push(interval[1]);
+
+    }
+
+    return minHeap.size();
+
+  }
+
+};
+
