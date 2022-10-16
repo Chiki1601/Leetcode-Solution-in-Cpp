1fb3f053087188ccde8e20c8e78216c0d467f17a
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-10-16 16:57:06 +0530

    Create 2443.cpp

---
+++ 2401-2500/2443.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool sumOfNumberAndReverse(int num) {
+        for(int i = 0; i <= num; ++i){
+            int n = i;
+            int r = 0;
+            while(n){
+                r = r*10 + n%10;
+                n = n/10;
+            }
+            if(r + i == num) return true;
+        }
+        return false;
+    }
+};
