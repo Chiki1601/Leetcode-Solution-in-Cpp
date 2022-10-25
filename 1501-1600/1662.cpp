6ddab08775fa166a4aca8ed8b8ce2c8b4acaf031
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-10-25 16:09:01 +0530

    Create 1662.cpp

---
+++ 1501-1600/1662.cpp
@@ -0,0 +1,8 @@
+class Solution 
+{
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) 
+    {
+        return reduce(word1.cbegin(), word1.cend()) == reduce(word2.cbegin(), word2.cend());
+    }
+};
