84ac8da921969d1917654c9357c56dcc6477cab1
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-12-11 19:59:30 +0530

    Create 124.cpp

---
+++ 1-500/124.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+ public:
+  int maxPathSum(TreeNode* root) {
+    int ans = INT_MIN;
+    maxPathSumDownFrom(root, ans);
+    return ans;
+  }
+
+ private:
+  // root->val + 0/1 of its subtrees
+  int maxPathSumDownFrom(TreeNode* root, int& ans) {
+    if (!root)
+      return 0;
+
+    const int l = max(0, maxPathSumDownFrom(root->left, ans));
+    const int r = max(0, maxPathSumDownFrom(root->right, ans));
+    ans = max(ans, root->val + l + r);
+    return root->val + max(l, r);
+  }
+};
