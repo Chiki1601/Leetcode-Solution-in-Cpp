9283907851dc9c47140edbe88c2a061aaae40c84
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-01-15 13:00:35 +0530

    Create 2538.cpp

---
+++ 2501-3000/2538.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long maxOutput(int n, vector<vector<int>>& edges, vector<int>& price) {
+        vector<vector<int>> g(n);
+        for(auto& e : edges) {
+            g[e[0]].push_back(e[1]);
+            g[e[1]].push_back(e[0]);
+        }
+        long long ans = 0;
+        function<vector<long long>(int, int)> dfs = [&] (int now, int pre) ->vector<long long> {
+            //cur_max[0] :max pathsum
+            //cur_max[1] :max (pathsum - end point)
+            vector<long long> cur_max = {price[now], 0};
+            for(auto& nei : g[now]) {
+                if(nei != pre) {
+                    auto&& sub = dfs(nei, now);
+                    ans = max(ans, (long long)cur_max[0] + sub[1]);
+                    ans = max(ans, (long long)cur_max[1] + sub[0]);
+                    cur_max[0] = max(cur_max[0], sub[0] + price[now]);
+                    cur_max[1] = max(cur_max[1], sub[1] + price[now]);
+                }
+            }
+            return cur_max;
+        };
+        dfs(0, -1);
+        return ans;
+    }
+};
