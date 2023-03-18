520488b1125aa6d5cb58c20ac83b4f49667868e9
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-03-18 08:57:51 +0530

    Create 1472.cpp

---
+++ 1401-1500/1472.cpp
@@ -0,0 +1,38 @@
+class BrowserHistory {
+ public:
+  BrowserHistory(string homepage) {
+    visit(homepage);
+  }
+
+  void visit(string url) {
+    if (++index < urls.size())
+      urls[index] = url;
+    else
+      urls.push_back(url);
+    lastIndex = index;
+  }
+
+  string back(int steps) {
+    index = max(0, index - steps);
+    return urls[index];
+  }
+
+  string forward(int steps) {
+    index = min(lastIndex, index + steps);
+    return urls[index];
+  }
+
+ private:
+  vector<string> urls;
+  int index = -1;
+  int lastIndex = -1;
+};
+
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory* obj = new BrowserHistory(homepage);
+ * obj->visit(url);
+ * string param_2 = obj->back(steps);
+ * string param_3 = obj->forward(steps);
+ */
