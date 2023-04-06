2785bd3cf8bf85053b79d25189f3ca12d9ae36ac
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-04-06 09:20:11 +0530

    Update 2608.cpp

--- 2501-3000/2608.cpp
+++ 2501-3000/2608.cpp
@@ -1,31 +1,48 @@
-class Solution {
+class Solution
+{
 public:
-       int findShortestCycle(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> G(n);
-        for (auto& e : edges) {
-            G[e[0]].push_back(e[1]);
-            G[e[1]].push_back(e[0]);
-        }
-        int inf = 10000, res = inf;
-        auto root = [&](int i) {
-            vector<int> dis(n, inf);
-            dis[i] = 0;
-            queue<int> bfs{{i}};
-            while (!bfs.empty()) {
-                auto i = bfs.front(); bfs.pop();
-                for (auto j : G[i]) {
-                    if (dis[j] == inf) {
-                        dis[j] = 1 + dis[i];
-                        bfs.push(j);
-                    } else if (dis[i] <= dis[j]) {
-                        return dis[i] + dis[j] + 1;
-                    }
+    
+    int ans = INT_MAX;
+
+    void bfs(int src,vector<int> adj[],int n){
+            vector<int> distance(n,(1e9));
+            vector<int> parent(n, -1);
+            
+            queue<int> q;
+            distance[src] = 0;
+            q.push(src);
+            while (!q.empty()) {
+                int node = q.front();
+                q.pop();
+                for (auto neigh : adj[node]) {
+                    if (distance[neigh] == (int)(1e9)) {
+                        distance[neigh] = 1 + distance[node];
+                        parent[neigh] = node;
+                        q.push(neigh);
+                }
+                else if (parent[node] != neigh && parent[neigh] != node)
+                    ans = min(ans, distance[neigh] + distance[node] + 1);
                 }
             }
-            return inf;
-        };
-        for (int i = 0; i < n; ++i)
-            res = min(res, root(i));
-        return res < inf ? res : -1;
+    }
+    int findShortestCycle(int n, vector<vector<int>> &edges)
+    {
+
+        vector<int> adj[n + 1];
+        for (auto it : edges)
+        {
+            int u = it[0];
+            int v = it[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+      
+        for (int i = 0; i < n; i++)
+        {
+            bfs(i,adj,n);
+        }
+        if (ans == INT_MAX)
+            return -1;
+        return ans;
     }
 };
