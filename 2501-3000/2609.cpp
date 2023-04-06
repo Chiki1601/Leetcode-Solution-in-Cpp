d5c60a334a2101cc822ec8e7f20f8f8200575acf
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-04-06 09:23:02 +0530

    Create 2609.cpp

---
+++ 2501-3000/2609.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findTheLongestBalancedSubstring(string s) {
+        int res = 0;
+        string temp = "01";
+        while(temp.size() <= s.size()){
+            if(s.find(temp) != string::npos) 
+                res = temp.size();
+            temp = '0' + temp + '1';
+        }
+        return res;
+    }
+};
