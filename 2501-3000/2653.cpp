e351b66f4e65caa4a9f02fd1c64873c564823a9b
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-04-25 09:02:06 +0530

    Create 2653.cpp

---
+++ 2501-3000/2653.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+  vector<int> getSubarrayBeauty(vector<int>& nums, int k, int x) {
+    vector<int> ans(nums.size() - k + 1), counter(50);
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] < 0) counter[nums[i] + 50]++;
+        if (i - k >= 0 && nums[i - k] < 0) counter[nums[i - k] + 50]--;
+        if (i - k + 1 < 0) continue;
+        for (int j = 0, count = 0; j < 50; j++) {
+            count += counter[j];
+            if (count >= x) {
+                ans[i - k + 1] = j - 50;
+                break;
+            }
+        }
+    }
+    return ans;
+}
+};
