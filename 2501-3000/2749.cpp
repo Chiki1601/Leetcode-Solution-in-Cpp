d13f3e6e0e47c08780531358369fd18a504197f8
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-06-29 09:01:14 +0530

    Create 2749.cpp

---
+++ 2501-3000/2749.cpp
@@ -0,0 +1,27 @@
+class Solution { 
+public: 
+    int countBits(long long num) { 
+        int count = 0; 
+        while (num > 0) { 
+            count += num & 1; 
+            num >>= 1; 
+        } 
+        return count; 
+    } 
+
+    int makeTheIntegerZero(int num1, int num2) { 
+        if (num1 < num2) { 
+            return -1; 
+        } 
+ 
+        for (int steps = 0; steps <= 100; steps++) { 
+            long long diff = num1 - 1LL*(num2) * steps; 
+            int bits = countBits(diff); 
+            if (bits <= steps && steps <= diff) { 
+                return steps; 
+            } 
+        } 
+ 
+        return -1; 
+    }
+};
