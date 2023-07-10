cddae736339d9d438d32ed79e24114bad65dced1
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-07-10 08:47:41 +0530

    Create 111.cpp

---
+++ 1-500/111.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (root==NULL) 
+            return 0;
+
+        int mndepth = 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int size = q.size();
+            mndepth++;
+          while (size--) {
+                TreeNode* cur = q.front(); 
+                q.pop();
+                if (cur->left)
+                    q.push(cur->left);
+                if (cur->right)
+                    q.push(cur->right);
+                if (cur->left == NULL && cur->right == NULL) {
+                    return mndepth;
+                }
+            }
+        }
+        return mndepth;
+    }
+};
