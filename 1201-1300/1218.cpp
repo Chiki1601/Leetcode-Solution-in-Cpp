8e6a3059d8385107b1136139b72050cf1f1d122c
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-07-14 08:21:36 +0530

    Update 1218.cpp

--- 1201-1300/1218.cpp
+++ 1201-1300/1218.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
-    int helper(int index, int prev, vector<int>& arr, int diff){
-        int n = arr.size();
-        if(index >= n){
-            return 0;
+    int longestSubsequence(vector<int>& nums, int difference) {
+        unordered_map<int, int> dp;
+        int max_length = 1;
+
+        for (int num : nums) {
+            if (dp.find(num - difference) != dp.end()) {
+                dp[num] = dp[num - difference] + 1;
+            } else {
+                dp[num] = 1;
+            }
+
+            max_length = max(max_length, dp[num]);
         }
 
-        int take = 0;
-        int notake = 0;
-        if(prev == -10000){
-            notake = helper(index + 1, prev, arr, diff);
-            take = 1 + helper(index + 1, arr[index], arr, diff);
-        }
-        else{
-            notake = helper(index + 1, prev, arr, diff);
-            if(arr[index] - prev == diff){
-                take = 1 + helper(index + 1, arr[index], arr, diff);
-            }
-        }
-        return max(take, notake);
-    }
-    int longestSubsequence(vector<int>& arr, int difference) {
-        int n = arr.size();
-        return helper(0, -10000, arr, difference);
+        return max_length;
     }
 };
