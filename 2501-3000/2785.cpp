b372b9b78e059ca4915bf11a602ed66cac1cab4d
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-07-29 08:39:01 +0530

    Update 2785.cpp

--- 2501-3000/2785.cpp
+++ 2501-3000/2785.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
-    unordered_set<char> vowel{'a', 'e', 'i', 'o','u', 'A', 'E', 'I', 'O','U'};
-        
-    bool check(char c){
-        return (vowel.find(c) != vowel.end());
-    }
-    
     string sortVowels(string s) {
-        map<int, int> mp;
-        for(auto c: s) mp[c] += check(c);        //put all vowel in the map with frequency 
-        map<int, int>::iterator it = mp.begin(); //set an iterator to the begining of the map
-        for(int i = 0; i < s.size(); ++i){
-            if(check(s[i])) {                    //check for vowel letter then replace this with sorted vowel
-                s[i] = (*it).first;              //from the map get the first key(vowel) which is non zero
-                if(--(*it).second ==  0) it++;   //decrease the frequency of the vowel and if zero, then move iterator to next vowel
+        // All vowels
+        vector<char> vow = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        string ans;
+        // vector to store vowels
+        vector<char> store;
+        // Iterate the string
+        for(char c: s){
+            // If c is a vowel, append to store
+            if(find(vow.begin(), vow.end(), c) != vow.end()){
+                store.push_back(c);
             }
         }
-        return s;
+        // sort all vowels with increasing ASCII
+        sort(store.begin(), store.end());
+
+        int idx = 0;
+        for(char c: s){
+            // When a vowel appears, call the sorted vector
+            if(find(vow.begin(), vow.end(), c) != vow.end()){
+                ans += store[idx++];
+            }
+            else    ans += c;
+        }
+
+        return ans;
     }
 };
