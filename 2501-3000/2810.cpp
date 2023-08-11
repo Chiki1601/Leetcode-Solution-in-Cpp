9e251c1922661e27b46d6b2831b72eb358e30645
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-08-12 03:05:38 +0530

    Create 2810.cpp

---
+++ 2501-3000/2810.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string finalString(string s) {
+        string res = "";
+        for(auto c : s){
+            if(c == 'i'){
+                if(res.size()){
+                    reverse(res.begin(), res.end());
+                }
+                
+            }else
+             res+=c;
+        }
+        return res;
+    }
+};
