2b4e3fbe65e8e4fbf93a306fbe6d644cfda12b7d
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-08-13 13:45:14 +0530

    Create 2816.cpp

---
+++ 2501-3000/2816.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* cur = head;
+        ListNode* temp = NULL;
+        while(cur) {
+            temp = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = temp;
+        }
+        return prev;
+    }
+public:
+    ListNode* doubleIt(ListNode* head) {
+        if(!head) return NULL;
+        ListNode* head2 = reverse(head);
+        ListNode* dummy = new ListNode(0);
+        ListNode* temp = dummy;
+        int carry = 0;
+        while(head2 != NULL || carry) {
+            int sum = 0;
+            if(head2) {
+                sum += (head2->val * 2);
+                head2 = head2->next;
+            }
+            
+            sum += carry;
+            
+            carry = sum / 10;
+            ListNode* curr = new ListNode(sum % 10);
+            temp->next = curr;
+            temp = temp->next;
+        }
+        
+        return reverse(dummy->next);
+    }
+};
