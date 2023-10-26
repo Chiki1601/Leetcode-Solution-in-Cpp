879e6027d4ca054cb5b4f44771914e79b16f0c16
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-10-26 16:36:12 +0530

    Create 2903.cpp

---
+++ 2501-3000/2903.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> findIndices(vector<int>& nums, int indexDifference, int valueDifference) {
+    for(int i = 0; i + indexDifference < nums.size(); ++i){
+        for(int j = i + indexDifference; j < nums.size(); ++j){
+            if(abs(nums[i] - nums[j]) >= valueDifference) return {i, j};
+        }
+    }
+    return {-1, -1};
+}
+};
