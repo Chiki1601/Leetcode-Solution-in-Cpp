165ee888a8c042d8f46889f160b5e36e645f5af7
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2024-02-07 11:59:33 +0530

    Update 3031.cpp

--- 3001-3500/3031.cpp
+++ 3001-3500/3031.cpp
@@ -1,10 +1,36 @@
-class Solution {
+class Solution 
+{
 public:
-  int minimumTimeToInitialState(string word, int k) {
-    auto z = z_function(word);
-    for (int i = k, n = word.size(); i < n; i += k)
-        if (z[i] == n - i)
-            return i / k;
-    return (word.size() + k - 1) / k;
-}
+    int minimumTimeToInitialState(string word, int k) 
+    {
+        int n = word.size();
+        vector<int>z(n);
+        int l = 0, r = 0;
+
+        for(int i = 1; i < n; i++)
+        {
+            if(i < r) z[i] = min(r-i, z[i-l]); 
+            while(i+z[i] < n && word[z[i]] == word[i+z[i]]) z[i]++;
+
+            if(i+z[i] > r)
+            {
+                l = i;
+                r = i+z[i];
+            }
+        }
+
+        int ans = 0;
+        for(int i = 1; i < n; i++)
+        {
+            if(z[i] == n-i && i %k == 0)
+            {
+                ans = (i+k-1)/k;
+                break;
+            }
+
+            if(i == n-1) ans = (n+k-1)/k;
+        }
+
+        return ans;
+    }
 };
