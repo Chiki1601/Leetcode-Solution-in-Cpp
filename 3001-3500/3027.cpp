474c0f7317e2a86156eea24e0661435648a8952f
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2024-02-07 11:58:21 +0530

    Update 3027.cpp

--- 3001-3500/3027.cpp
+++ 3001-3500/3027.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
+    static bool comp(vector<int>& a, vector<int>& b){
+        if(a[0]==b[0])return a[1]>b[1];
+        return a[0]<b[0];
+    }
     int numberOfPairs(vector<vector<int>>& v) {
         int n=v.size();
+        sort(v.begin(),v.end(),comp);
         int ans=0;
-        for(int i=0;i<n;i++) {
-            int x1=v[i][0],y1=v[i][1];
-            for(int j=0;j<n;j++) {
-                if(i==j)
-                    continue;
-                int x2=v[j][0],y2=v[j][1];
-                int f=0;
-                if(y1>=y2 && x1<=x2) {
-                    for(int k=0;k<n;k++) {
-                        if(k==i || k==j)
-                            continue;
-                        int x3=v[k][0],y3=v[k][1];
-                        if(x3>=x1 && x3<=x2 && y3>=y2 && y3<=y1) {
-                            f=1;
-                            break;
-                        }
-                    }
-                    if(f==0)
-                        ans++;
+        for(int i=0; i<n; i++){
+            int minY=INT_MIN;
+            for(int j=i+1; j<n; j++){
+                if(v[j][1]<=v[i][1]){
+                    if(v[j][1]>minY)ans++;
+                    minY=max(minY, v[j][1]);
                 }
-                
             }
         }
         return ans;
