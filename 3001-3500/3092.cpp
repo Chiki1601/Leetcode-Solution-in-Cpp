3615f4f45371d8c5078c1f9ab60ac5fd1ff13569
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2024-03-25 23:42:01 +0530

    Update 3092.cpp

--- 3001-3500/3092.cpp
+++ 3001-3500/3092.cpp
@@ -1,42 +1,23 @@
-#define ll long long
 class Solution {
 public:
-    vector<long long> mostFrequentIDs(vector<int>& id, vector<int>& freq) {
-        unordered_map<ll,ll> um;
-        
-        vector<ll> ans;
-        
-        ll curmax=0;
-        
-        for(int i=0;i<id.size();i++){
-            if(freq[i]<0){
-                // if negative hain then frequency add kro
-                // dont substract it my friend as -(-freq[i])= +freq[i]
-                um[id[i]] += freq[i];
-
-                // agr koi freq km hui hain toh we dont know curmax
-                // toh curmax ko 0 kr do and loop lgao map me
-                curmax = 0;
-                if(um[id[i]]<=0){um.erase(id[i]);}
-
-                // this loop help to get max freq again
-                for(auto &val:um){
-                    curmax = max(curmax,val.second);
+    vector<long long> mostFrequentIDs(vector<int>& v, vector<int>& f) {
+        priority_queue<pair<long long,int>>q;
+        int n=v.size();
+        map<int,long long>m;
+        vector<long long>ans;
+        for(int i=0;i<n;i++) {
+            m[v[i]]+=f[i];
+            q.push({m[v[i]],v[i]});
+            while(true) {
+                pair<long long,int>p=q.top();
+                if(p.first==m[p.second]) {
+                    ans.push_back(p.first);
+                    break;
                 }
-                ans.push_back(curmax);
-            }
-            else{
-                // agr positive hain toh max element hmesha you can
-                // find with taking max of last_max and current 
-                // added frequency
-                um[id[i]] += freq[i];
-
-                curmax=max(curmax,um[id[i]]);
-
-                ans.push_back(curmax);
+                else
+                    q.pop();
             }
         }
-                
         return ans;
     }
 };
