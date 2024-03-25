c955b992a97dbdc0ea618daef0adb2b31df874f1
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2024-03-25 23:40:03 +0530

    Update 3092.cpp

--- 3001-3500/3092.cpp
+++ 3001-3500/3092.cpp
@@ -1,19 +1,42 @@
+#define ll long long
 class Solution {
 public:
-  vector<long long> mostFrequentIDs(vector<int>& nums, vector<int>& f) {
-    vector<long long> ans;
-    unordered_map<int, long long> idToFreqMap;          // id to freq of each id unordered map
-    map<long long, int> freqToIdCountMap;               // freq to id's count map - keep track of number of ids have same freq
-    for(int i = 0; i < nums.size(); ++i){               // iterate over each item in nums
-        if(idToFreqMap[nums[i]]){                       // When freq is non-zero then remove its occurance from the freq to id map 
-            freqToIdCountMap[idToFreqMap[nums[i]]]--;   // first reduce the count of freq to id map of last freq
-            if(freqToIdCountMap[idToFreqMap[nums[i]]] == 0) freqToIdCountMap.erase(idToFreqMap[nums[i]]);      // when freq to id map have current freq occuarance as zero then remove it from the map
+    vector<long long> mostFrequentIDs(vector<int>& id, vector<int>& freq) {
+        unordered_map<ll,ll> um;
+        
+        vector<ll> ans;
+        
+        ll curmax=0;
+        
+        for(int i=0;i<id.size();i++){
+            if(freq[i]<0){
+                // if negative hain then frequency add kro
+                // dont substract it my friend as -(-freq[i])= +freq[i]
+                um[id[i]] += freq[i];
+
+                // agr koi freq km hui hain toh we dont know curmax
+                // toh curmax ko 0 kr do and loop lgao map me
+                curmax = 0;
+                if(um[id[i]]<=0){um.erase(id[i]);}
+
+                // this loop help to get max freq again
+                for(auto &val:um){
+                    curmax = max(curmax,val.second);
+                }
+                ans.push_back(curmax);
+            }
+            else{
+                // agr positive hain toh max element hmesha you can
+                // find with taking max of last_max and current 
+                // added frequency
+                um[id[i]] += freq[i];
+
+                curmax=max(curmax,um[id[i]]);
+
+                ans.push_back(curmax);
+            }
         }
-        idToFreqMap[nums[i]] += freq[i];                // update the id to freq map with the current freq
-        freqToIdCountMap[idToFreqMap[nums[i]]]++;       // update the freq to id count map as per latest freq of id
-        ans.push_back(freqToIdCountMap.rbegin()->first);
+                
+        return ans;
     }
-    return ans;
-}     
-   
 };
