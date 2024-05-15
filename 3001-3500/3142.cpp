80d427243aca6320db3b73bfedd93d4c27a1c9c9
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2024-05-15 16:51:46 +0530

    Create 3142.cpp

---
+++ 3001-3500/3142.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool satisfiesConditions(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        for (int i = 0; i + 1 < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] != grid[i+1][j]) {
+                    return false;
+                }
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j + 1 < m; ++j) {
+                if (grid[i][j] == grid[i][j+1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
