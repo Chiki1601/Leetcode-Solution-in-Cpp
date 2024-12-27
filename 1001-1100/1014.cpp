237317e3eb5c8a78dd8a9e926461a562f607500d
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2024-12-27 09:42:24 +0530

    Create 1014.cpp

---
+++ 1001-1100/1014.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+  int maxScoreSightseeingPair(vector<int>& values) {
+    int max = 0, score = values[0];
+    for (int i = 1; i < values.size(); ++i) {
+      --score;
+      if (score + values[i] > max) max = score + values[i];
+      if (values[i] > score) score = values[i];
+    }
+    return max;
+  }
+};
