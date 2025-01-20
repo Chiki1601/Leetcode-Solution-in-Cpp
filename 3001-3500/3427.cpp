4f800c235c6d5a3e3e3b32c66a8a04ab4ca18c25
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2025-01-20 10:17:04 +0530

    Update 3427.cpp

--- 3001-3500/3427.cpp
+++ 3001-3500/3427.cpp
@@ -1,9 +1,20 @@
 class Solution {
-public:
+   public:
     int subarraySum(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> diff(n + 1, 0);
+        for (int i = 0; i < n; ++i) {
+            int start = max(0, i - nums[i]);
+            ++diff[start];
+            --diff[i + 1];
+        }
         for (int i = 1; i < n; ++i) {
-    diff[i] += diff[i-1];
-}
-        
+            diff[i] += diff[i - 1];
+        }
+        int subSum = 0;
+        for (int i = 0; i < n; ++i) {
+            subSum += nums[i] * diff[i];
+        }
+        return subSum;
     }
 };
