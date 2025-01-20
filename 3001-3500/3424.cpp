ff9f8fd5a6e00e006ab8e0f191f746ccf2ad88c7
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2025-01-20 10:16:27 +0530

    Update 3424.cpp

--- 3001-3500/3424.cpp
+++ 3001-3500/3424.cpp
@@ -1,32 +1,15 @@
 class Solution {
 public:
-    long long cal(vector<int>& arr, vector<int>& brr){
-        int n = arr.size();
-        long long diff = 0;
-        for(int i=0; i<n; i++){
-            diff += abs(arr[i] - brr[i]);
+        long long minCost(vector<int>& a, vector<int>& b, long long k) {
+        long long res1 = 0, res2 = 0, n = a.size();
+        for (int i = 0; i < n; ++i) {
+            res1 += abs(a[i] - b[i]);
         }
-        return diff;
-    }
-
-    long long minCost(vector<int>& arr, vector<int>& brr, long long k){
-        //Calculating cost without rearranging
-        long long ans = cal(arr, brr);
-
-        //Rearranging considering subarray of size 1.
-        sort(arr.begin(), arr.end());
-        sort(brr.begin(), brr.end());
-
-        //Calculating cost after rearranging
-        ans = min(ans, cal(arr, brr)+k);
-        return ans;
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+        for (int i = 0; i < n; ++i) {
+            res2 += abs(a[i] - b[i]);
+        }
+        return min(res1, res2 + k);
     }
 };
-
-//For Better Performance of code
-static const int kds = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 0;
-}()
