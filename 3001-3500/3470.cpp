e6ad6cfaaceecc104430b1395a111652b2432ecc
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2025-03-03 09:38:53 +0530

    Update 3470.cpp

--- 3001-3500/3470.cpp
+++ 3001-3500/3470.cpp
@@ -1,64 +1,82 @@
-class Solution {
-public:
- vector<long long> fact(101, LLONG_MAX);
-
-void init(){
-    if(fact[0] == 1) return;
-    fact[0] = 1;
-    for(int i = 1; i <= 20; i++)
-        fact[i] = i * fact[i - 1];
-}
 
 class Solution {
 public:
-    int n, numsLeft[2];
-    vector<int> ans;
-    
-    long long calcPerms(int odd, int even){
-        long long fromOdd = fact[odd];
-        long long fromEven = fact[even];
-
-        long long totalPerms = LLONG_MAX;
-        if(log10(fromOdd) + log10(fromEven) <= 18)
-            totalPerms = fromOdd * fromEven;
-        
-        return totalPerms;
+    typedef long long ll;
+    const ll INF = 1000000000000000000LL;
+    ll helper(int a, int b) {
+        __int128 res = 1;
+        for (int i = 0; i < b; i++) {
+            res *= (a - i);
+            if (res > INF) return INF;
+        }
+        return (ll)res;
     }
-    //placeOdd == -1, can place anything here, (haven't started)
-    void constructArray(int placeOdd, array<vector<bool>, 2> &numsChosen, long long k){
-        if(numsLeft[0] + numsLeft[1] == 0)
-            return;
-        
-        long long nextPerms;
-        if(placeOdd == -1) nextPerms = calcPerms(numsLeft[1], numsLeft[0] - 1);
-        else nextPerms = calcPerms(numsLeft[1] - placeOdd, numsLeft[0] - !placeOdd);
-
-        for(int i = 1; i <= n; i++){
-            if(placeOdd == -1 || ((i & 1) == placeOdd)){
-                if(!numsChosen[i & 1][i]){
-                    if(k > nextPerms) k -= nextPerms;
-                    else{
-                        numsLeft[i & 1]--;
-                        ans.push_back(i);
-                        numsChosen[i & 1][i] = 1;
-                        constructArray(!(i & 1), numsChosen, k);
+    
+    ll solve(int odd, int even, int r, int req) {
+        if (r == 0) return 1;
+        int nodd, neven;
+        if (req == 1) {
+            nodd = (r + 1) / 2;
+            neven = r / 2;
+        } else {
+            neven = (r + 1) / 2;
+            nodd = r / 2;
+        }
+        if (odd < nodd || even < neven) return 0;
+        ll oddways = helper(odd, nodd);
+        ll evenways = helper(even, neven);
+        ll total = oddways;
+        if (evenways == 0 || total > INF / evenways) total = INF; 
+        else total *= evenways;
+        return total;
+    }
+    
+    vector<int> permute(int n, long long k) {
+        vector<int> ans;
+        bool first = false;
+        vector<bool> used(n + 1, false);
+        int odd = (n + 1) / 2, even = n / 2, last;
+        for (int i = 1; i <= n; i++) {
+            if (used[i]) continue;
+            int odd2 = odd, even2 = even;
+            int cp = i & 1;
+            int next = (cp == 1 ? 0 : 1);
+            if (cp == 1) odd2--; else even2--;
+            int r = n - 1;
+            ll cnt = solve(odd2, even2, r, next);
+            if (k > cnt) k -= cnt; 
+            else {
+                ans.push_back(i);
+                used[i] = true;
+                odd = odd2; even = even2;
+                last = cp;
+                first = true;
+                break;
+            }
+        }
+        if (!first) return {};
+        for (int z = 1; z < n; z++) {
+            bool taken = false;
+            for (int j = 1; j <= n; j++) {
+                if (!used[j] && ((j & 1) != last)) {
+                    int odd2 = odd, even2 = even;
+                    int cp = j & 1;
+                    if (cp == 1) odd2--; else even2--;
+                    int r = n - (z + 1);
+                    int next = (cp == 1 ? 0 : 1);
+                    ll cnt2 = solve(odd2, even2, r, next);
+                    if (k > cnt2) k -= cnt2; else {
+                        ans.push_back(j);
+                        used[j] = true;
+                        odd = odd2; even = even2;
+                        last = cp;
+                        taken = true;
                         break;
                     }
                 }
             }
+            if (!taken) return {};
         }
-    }
-    vector<int> permute(int n, long long k) {
-        init();
-        this -> n = n;
-        numsLeft[1] = (n + 1) / 2, numsLeft[0] = n / 2;
-        
-        array<vector<bool>, 2> numsChosen = {vector<bool> (n + 1, false), vector<bool> (n + 1, false)};
-        // if evenDigits, can start from both odd or even
-        int placeOdd = n % 2 ? 1 : -1;
-        constructArray(placeOdd, numsChosen, k);
-        
         return ans;
     }
-};
 };
