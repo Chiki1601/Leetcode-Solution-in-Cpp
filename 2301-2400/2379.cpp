efe1f975a9c4d59696744c84a1c2e588e2dd9085
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2025-03-10 11:22:35 +0530

    Update 2379.cpp

--- 2301-2400/2379.cpp
+++ 2301-2400/2379.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     int minimumRecolors(string blocks, int k) {
-              int ans = INT_MAX;
-              int n = blocks.size();
-              for(int i = 0; i < n; i++) {
-                  for(int j = i; j < n; j++) {
-                          if(j - i + 1 == k) {
-                              int w = 0;
-                              for(int k = i; k <= j; k++) {
-                                  if(blocks[k] == 'W') {
-                                      w++;
-                                  }
-                              }   
-                              ans = min(ans, w);
-                          }
-                  }
-              }
+        int black_count = 0, ans = INT_MAX;
+        
+        for (int i = 0; i < blocks.size(); i++) {
+            if (i - k >= 0 && blocks[i - k] == 'B') black_count--;
+            if (blocks[i] == 'B') black_count++;
+            ans = min(ans, k - black_count);
+        }
         
         return ans;
     }
