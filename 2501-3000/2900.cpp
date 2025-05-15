457d50ffb4e624e735a930e9568ab60deccc18db
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2025-05-15 12:54:17 +0530

    Update 2900.cpp

--- 2501-3000/2900.cpp
+++ 2501-3000/2900.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
-   
-vector<string> getWordsInLongestSubsequence(int n, vector<string>& words, vector<int>& groups) {
-    vector<string> ans;
-    int last = groups[0];
-    ans.push_back(words[0]);
-    for(int i = 1; i < words.size(); ++i){
-        if(last != groups[i]){
-            last = groups[i];
-            ans.push_back(words[i]);
+    vector<string> getLongestSubsequence(vector<string>& w, vector<int>& g) {
+        vector<string> res = {w[0]};
+        for (int i = 1; i < g.size(); ++i) {
+            if (g[i] != g[i - 1]) res.push_back(w[i]);
         }
+        return res;
     }
-    return ans;
-}
 };
