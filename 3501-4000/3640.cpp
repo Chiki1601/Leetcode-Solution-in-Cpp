05c12f755441872bad6b039cec6dbe819c382110
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2025-08-06 09:52:03 +0530

    Update 3640.cpp

--- 3501-4000/3640.cpp
+++ 3501-4000/3640.cpp
@@ -1,26 +1,47 @@
-long long maxSumTrionic(vector<int>& nums) {
-    long long n = nums.size(), res = LLONG_MIN, psum = nums[0];
-    for (int l = 0, p = 0, q = 0, r = 1; r < nums.size(); ++r) {
-        psum += nums[r];
-        if (nums[r - 1] == nums[r]) {
-            l = r;
-            psum = nums[r];
+using ll=long long int;
+class Solution {
+public:
+    long long maxSumTrionic(vector<int>& nums) {
+        ll n=nums.size();
+        ll ans=-1e17;
+        vector<ll>pref(n,0);
+        pref[0]=nums[0];
+        for(int i=1;i<n;i++)pref[i]=pref[i-1]+nums[i];
+        vector<ll>inc(n,1);
+        vector<ll>dec(n,1);
+        ll ct1=1,ct2=1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                ct1++;
+                ct2=1;
+            }
+            else if(nums[i]>nums[i+1]){
+                ct1=1;
+                ct2++;
+            }
+            else{
+                ct1=1;
+                ct2=1;
+            }
+            inc[i]=ct1;
+            dec[i]=ct2;
         }
-        else if (nums[r - 1] > nums[r]) {
-            if (r > 1 && nums[r - 2] < nums[r - 1]) { // flip
-                p = r - 1;
-                while (l < q)
-                    psum -= nums[l++];    
-                while (l + 1 < p && nums[l] < 0)
-                    psum -= nums[l++];
+        for(int l=0;l<n-3;l++){
+            if(inc[l]>1){
+                int p=l+inc[l]-1;
+                if(dec[p]>1){
+                    int q=p+dec[p]-1;
+                    if(inc[q]>1){
+                        int r1=q+1;
+                        int r2=q+inc[q]-1;
+                        ll sum1=(pref[r1]-pref[l]+nums[l]);
+                        ll sum2=(pref[r2]-pref[l]+nums[l]);
+                        ll val=max(sum1,sum2);
+                        ans=max(ans,val);
+                    }
+                }
             }
         }
-        else {
-            if (r > 1 && nums[r - 2] > nums[r - 1]) // flip
-                q = r - 1;
-            if (l < p && p < q)
-                res = max(res, psum);
-        }
+        return ans;
     }
-    return res;
-}
+};
