6f7d010d709a9ed54716b5c03743f702d885366a
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2025-09-29 10:08:34 +0530

    Update 3694.cpp

--- 3501-4000/3694.cpp
+++ 3501-4000/3694.cpp
@@ -1,36 +1,17 @@
-#include <bits/stdc++.h>
-using namespace std;
-
+typedef long long ll;
 class Solution {
 public:
-    int distinctPoints(string s, int k) {
-        int n = s.size();
-        vector<int> prex(n+1,0), prey(n+1,0);
-        for (int i = 0; i < n; ++i) {
-            prex[i+1] = prex[i];
-            prey[i+1] = prey[i];
-            char c = s[i];
-            if (c == 'U') prey[i+1]++;
-            else if (c == 'D') prey[i+1]--;
-            else if (c == 'L') prex[i+1]--;
-            else prex[i+1]++;
+    int climbStairs(int n, vector<int>& costs) {
+        vector<ll> dp(n+1, LLONG_MAX/2);
+        vector<int> nums(n+1, 0);
+        for (int i = 1; i <= n; i++) nums[i] = costs[i-1];
+        
+        dp[0] = 0; // Base: no cost at step 0
+        for (int i = 1; i <= n; i++) {
+            if (i-1 >= 0) dp[i] = min(dp[i], dp[i-1] + nums[i] + 1LL*(1*1));
+            if (i-2 >= 0) dp[i] = min(dp[i], dp[i-2] + nums[i] + 1LL*(2*2));
+            if (i-3 >= 0) dp[i] = min(dp[i], dp[i-3] + nums[i] + 1LL*(3*3));
         }
-        vector<int> sufx(n+1,0), sufy(n+1,0);
-        for (int i = n-1; i >= 0; --i) {
-            sufx[i] = sufx[i+1];
-            sufy[i] = sufy[i+1];
-            char c = s[i];
-            if (c == 'U') sufy[i]++;
-            else if (c == 'D') sufy[i]--;
-            else if (c == 'L') sufx[i]--;
-            else sufx[i]++;
-        }
-        set<pair<int,int>> ss;
-        for (int i = 0; i <= n - k; ++i) {
-            int x = prex[i] + sufx[i + k];
-            int y = prey[i] + sufy[i + k];
-            ss.insert({x, y});
-        }
-        return ss.size();
+        return (int)dp[n];
     }
 };
