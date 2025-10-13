cf728676029360d02a2e7f998c2d46ee969e9495
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2025-10-13 10:03:10 +0530

    Update 3707.cpp

--- 3501-4000/3707.cpp
+++ 3501-4000/3707.cpp
@@ -1,12 +1,13 @@
 class Solution {
-    public boolean scoreBalance(String s) {
-        int total = 0;
-        for (char c : s.toCharArray()) total += c - 'a' + 1;
-        int prefix = 0;
-        for (char c : s.toCharArray()) {
-            prefix += c - 'a' + 1;
-            if (2 * prefix == total) return true;
+public:
+    bool scoreBalance(string s) {
+        int c = 0;
+        for (auto i : s) c += i - 'a' + 1;
+        int x = 0;
+        for (auto i : s) {
+            x += i - 'a' + 1;
+            if (x * 2 == c) return true;
         }
         return false;
     }
-}
+};
