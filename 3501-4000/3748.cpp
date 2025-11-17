5991af251b4e89947eab34c60c09587c7bf9ae06
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2025-11-17 15:17:41 +0530

    Update 3748.cpp

--- 3501-4000/3748.cpp
+++ 3501-4000/3748.cpp
@@ -1,109 +1,77 @@
 class Solution {
-    public long[] countStableSubarrays(int[] nums, int[][] queries) {
-
-        int n = nums.length;
-        int q = queries.length;
-
-        long[] len = new long[n];
-        len[0] = 1;
-        for (int i = 1; i < n; i++) {
-            if (nums[i - 1] <= nums[i]) len[i] = len[i - 1] + 1;
-            else len[i] = 1;
-        }
-
-        // prefix sum of lengths
-        long[] pref = new long[n + 1];
-        for (int i = 0; i < n; i++) pref[i + 1] = pref[i] + len[i];
-
-        // index[i] = farthest extension of the non-decreasing streak starting at i
-        int[] index = new int[n];
-        int idx = n - 1;
-        index[n - 1] = n - 1;
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] <= nums[i + 1]) index[i] = idx;
-            else {
-                idx = i;
-                index[i] = idx;
-            }
-        }
-
-        long[] ans = new long[q];
-
-        for (int qi = 0; qi < q; qi++) {
-            int l = queries[qi][0];
-            int r = queries[qi][1];
-
-            // find where streak from l breaks
-            int right = Math.min(index[l], r);
-
-            long diff = right - l + 1;
-            long sum = diff * (diff + 1) / 2;
-
-            long after = 0;
-            if (right < r) {
-                after = pref[r + 1] - pref[right + 1];
-            }
-
-            ans[qi] = sum + after;
-        }
-
-        return ans;
-    }
-}
-
-
-# Code
-```C++
-
-class Solution {
 public:
+    #define ll long long
+    #define debug(x) cout<<#x<<" is: "<<x<<endl;
+    
     vector<long long> countStableSubarrays(vector<int>& nums, vector<vector<int>>& queries) {
+        ll n = nums.size();
+        vector<ll> ans;
+        int last = nums[0];
+        nums[0] = 1;
+
+        // We transform nums into lengths inside block for convenience
+        vector<ll> block;
+        vector<pair<ll,ll>> dj;
+        unordered_map<ll,ll> mp;//index-->block number
+        ll bl = 0;
+        mp[0] = bl;
+        ll len = 1;
+
+        // Build blocks of non-decreasing segments
+        for(int i=1; i<n; i++){
+            if(nums[i]>=last){
+                last = nums[i];
+                mp[i] = bl;
+                len++;
+            }else{
+                block.push_back(len*(len+1)/2);
+                dj.push_back({i-1-len+1,i-1});
+                len = 1;
+                last = nums[i];
+                bl++;
+                mp[i] = bl;
+            }
+        }
+        block.push_back(len*(len+1)/2);
+        dj.push_back({n-1-len+1,n-1});
         
-        int n = nums.size();
-        int q = queries.size();
-
-        vector<long long> len(n);
-        len[0] = 1;
-        for (int i = 1; i < n; i++) {
-            if (nums[i - 1] <= nums[i]) len[i] = len[i - 1] + 1;
-            else len[i] = 1;
+        ll sz = block.size();
+        // Prefix sum of block values
+        vector<ll> pref(sz);
+        pref[0] = block[0];
+        for(int i=1; i<sz; i++){
+            pref[i] = pref[i-1]+block[i];
         }
-
-        // prefix sum of len[]
-        vector<long long> pref(n + 1, 0);
-        for (int i = 0; i < n; i++) {
-            pref[i + 1] = pref[i] + len[i];
-        }
-
-        // index[i] = farthest index where non-decreasing streak starting at i continues
-        vector<int> index(n);
-        int idx = n - 1;
-        index[n - 1] = n - 1;
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] <= nums[i + 1]) index[i] = idx;
-            else {
-                idx = i;
-                index[i] = idx;
+        // Process queries
+        for(auto& x: queries){
+            ll l = x[0], r = x[1];
+            //block no. of l , block no. of r
+            ll lb = mp[l], rb = mp[r];
+            // If both inside one block
+            if(lb==rb){
+                ll len = r-l+1;
+                ans.push_back(len*(len+1)/2);
+                continue;
             }
-        }
-
-        vector<long long> ans(q);
-
-        for (int qi = 0; qi < q; qi++) {
-            int l = queries[qi][0];
-            int r = queries[qi][1];
-
-            int right = min(index[l], r);
-
-            long long diff = right - l + 1;
-            long long sum = diff * (diff + 1) / 2;
-
-            long long after = 0;
-            if (right < r) {
-                after = pref[r + 1] - pref[right + 1];
+            ll sm = 0;
+            // Full blocks between them
+            if(abs(lb-rb)>=2){
+                //lb+1 to rb-1 ka sum       
+                sm = pref[rb-1]-pref[lb];
             }
+                 
+            // Partial left block
+            auto& xx = dj[lb];
+            //l to x.second
+            ll len = xx.second-l+1;
+            sm += len*(len+1)/2;
 
-            ans[qi] = sum + after;
+            // Partial right block
+            auto& y = dj[rb];
+            //y.first to r
+            len = r-y.first+1;
+            sm += len*(len+1)/2;
+            ans.push_back(sm);
         }
 
         return ans;
