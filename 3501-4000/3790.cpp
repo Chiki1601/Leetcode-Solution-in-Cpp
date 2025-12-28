438b784e64e4114c079270bc636cd6744387bec6
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2025-12-28 14:55:24 +0530

    Create 3790.cpp

---
+++ 3501-4000/3790.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minAllOneMultiple(int k) {
+        if(k % 2 == 0 || k % 5 == 0) return - 1;
+        int rem = 0;
+        for(int i = 1; i <= k; i++) {
+            rem = (rem * 10 + 1) % k;
+            if(rem == 0) return i;
+        }
+        return -1;
+    }
+};
