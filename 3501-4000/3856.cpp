d2cd4b61bb665cb00771fe731a41c89dbede837f
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2026-03-02 11:02:32 +0530

    Update 3856.cpp

--- 3501-4000/3856.cpp
+++ 3501-4000/3856.cpp
@@ -1,55 +1,16 @@
 class Solution {
 public:
-    // brute force
-    bool check(int ind, string& s) {
-        for (int i = ind; i < s.length(); i++) {
-            char ch = s[i];
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
-                continue;
-            else
-                return false;
-        }
-        return true;
-    }
     string trimTrailingVowels(string s) {
-        for (int i = 0; i < s.length(); i++) {
-            bool canRemove = check(i, s);
-            if (canRemove)
-                return s.substr(0, i);
-        }
-        return s;
+        int n=s.size();
+        int i=n-1;
+        while(i>=0 && isVowel(s[i])) i--;
+        if(i<0) return "";
+        return s.substr(0,i+1);
     }
-    // better
-    string trimTrailingVowels(string s) {
-        stack<char> st;
-        for (auto it : s)
-            st.push(it);
-        while (!st.empty()) {
-            char ch = st.top();
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
-                st.pop();
-            else
-                break;
-        }
-        string ans = "";
-        while (!st.empty()) {
-            ans.push_back(st.top());
-            st.pop();
-        }
-        reverse(ans.begin(), ans.end());
-        return ans;
-    }
-    // optimal
-    string trimTrailingVowels(string s) {
-        int lastInd = s.length() - 1;
-        while (lastInd >= 0) {
-            char ch = s[lastInd];
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
-                lastInd--;
-            else
-                break;
-        }
-        int lenOfAns = lastInd + 1;
-        return s.substr(0, lenOfAns);
+
+    bool isVowel(char ch){
+        return ch=='a' ||
+        ch=='e' ||
+         ch=='i' || ch=='o' || ch=='u';
     }
 };
