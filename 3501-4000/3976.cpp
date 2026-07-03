bf8b655063f41538cecd671b5da4c29cb4337e2e
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2026-07-03 09:39:07 +0530

    Update 3976.cpp

--- 3501-4000/3976.cpp
+++ 3501-4000/3976.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    long long maxSubarraySum(vector<int>& nums, int k) {
-        long long kadane = 0,sum = 0;
-        for(auto &it : nums){
-            if(sum<0)sum = 0;
-            sum += 1LL * it;
-            kadane = max(kadane,sum);
+      long long maxSubarraySum(vector<int>& A, int k) {
+        long long res = -1e9, dp0 = 0, dpb = 0, dpc = 0, dp1 = 0;
+        for (long long a: A) {
+            long long b = a * k, c = a / k;
+            dp1 = max({dp1 + a, dpb + a, dpc + a});
+            dpb = max({dp0 + b, dpb + b, b});
+            dpc = max({dp0 + c, dpc + c, c});
+            dp0 = max(dp0 + a, a);
+            res = max({res, dp0, dpb, dpc, dp1});
         }
-        if(kadane>0)return k * kadane;
-        sort(nums.begin(),nums.end(),greater());
-        return ceil(nums[0]/k);
+        return res;
     }
 };
