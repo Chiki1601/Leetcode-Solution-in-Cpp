9c23e1a299c88d2b4a60a8546e7c241ef66e3183
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2026-08-18 09:29:40 +0530

    Update 4023.cpp

--- 4001-4500/4023.cpp
+++ 4001-4500/4023.cpp
@@ -1,101 +1,42 @@
 class Solution {
 public:
-    long long minPenalty(int n, int start, vector<int>& requests) {
-        vector<int> L, R;
-        for (int r : requests) {
-            if (r < start) L.push_back(r);
-            else if (r > start) R.push_back(r);
+    #define ll long long
+    ll func(vector<ll> &pos, vector<ll> &neg, ll ind1, ll ind2, ll fg,vector<vector<vector<ll>>> &dp){
+        if(ind1==pos.size()&& ind2==neg.size())
+        return 0;
+
+        if(dp[fg][ind1][ind2]!=-1)
+        return dp[fg][ind1][ind2];
+
+        ll cord;
+        if(fg)cord=pos[ind1-1];
+        else cord=neg[ind2-1];
+
+        ll ans=1e18;
+        if(ind1<pos.size())
+        ans=min(ans, (ll)(abs(cord-pos[ind1])*(pos.size()+neg.size()-ind1-ind2))+func(pos,neg,ind1+1,ind2,1,dp));
+
+        if(ind2<neg.size())
+        ans=min(ans, (ll)(abs(cord-neg[ind2])*(pos.size()+neg.size()-ind1-ind2))+func(pos,neg,ind1,ind2+1,0,dp));
+
+        return dp[fg][ind1][ind2]=ans;
+    }
+    long long elevatorRequests(int n, int start, vector<int>& v) {
+        sort(v.begin(),v.end());
+        // debug(v);
+        vector<ll> neg,pos;
+        ll nn=v.size();
+        for(ll i=0;i<nn;i++){
+            if(v[i]==start)
+            continue;
+            if(v[i]<start)neg.push_back(v[i]);
+            else pos.push_back(v[i]);
         }
-
-        sort(L.rbegin(), L.rend());
-        sort(R.begin(), R.end());
-
-        int p = L.size(), q = R.size();
-        vector<long long> d(p + 1), e(q + 1);
-        d[0] = 0;
-        for (int i = 1; i <= p; i++) d[i] = start - L[i - 1];
-        e[0] = 0;
-        for (int j = 1; j <= q; j++) e[j] = R[j - 1] - start;
-
-        const long long INF = LLONG_MAX / 4;
-
-        vector<vector<long long>> S0(p + 1, vector<long long>(q + 1, INF));
-        vector<vector<long long>> T0(p + 1, vector<long long>(q + 1, 0));
-        vector<vector<long long>> S1(p + 1, vector<long long>(q + 1, INF));
-        vector<vector<long long>> T1(p + 1, vector<long long>(q + 1, 0));
-
-        S0[0][0] = S1[0][0] = 0;
-
-        for (int i = 0; i <= p; i++) {
-            for (int j = 0; j <= q; j++) {
-                if (i == 0 && j == 0) continue;
-                long long K = (p - i) + (q - j);
-
-                if (i >= 1) {
-                    long long bestS = INF, bestT = 0;
-
-                    if (S0[i - 1][j] < INF) {
-                        long long step = d[i] - d[i - 1];
-                        long long T = T0[i - 1][j] + step;
-                        long long S = S0[i - 1][j] + T;
-                        if (S + K * T < bestS + K * bestT) {
-                            bestS = S;
-                            bestT = T;
-                        }
-                    }
-
-                    if (j >= 1 ? S1[i - 1][j] < INF : (i - 1 == 0)) {
-                        long long baseS = (j >= 1) ? S1[i - 1][j] : 0;
-                        long long baseT = (j >= 1) ? T1[i - 1][j] : 0;
-                        long long step = (j >= 1 ? e[j] : 0) + d[i];
-                        long long T = baseT + step;
-                        long long S = baseS + T;
-                        if (S + K * T < bestS + K * bestT) {
-                            bestS = S;
-                            bestT = T;
-                        }
-                    }
-
-                    S0[i][j] = bestS;
-                    T0[i][j] = bestT;
-                }
-
-                if (j >= 1) {
-                    long long bestS = INF, bestT = 0;
-
-                    if (S1[i][j - 1] < INF) {
-                        long long step = e[j] - e[j - 1];
-                        long long T = T1[i][j - 1] + step;
-                        long long S = S1[i][j - 1] + T;
-                        if (S + K * T < bestS + K * bestT) {
-                            bestS = S;
-                            bestT = T;
-                        }
-                    }
-
-                    if (i >= 1 ? S0[i][j - 1] < INF : (j - 1 == 0)) {
-                        long long baseS = (i >= 1) ? S0[i][j - 1] : 0;
-                        long long baseT = (i >= 1) ? T0[i][j - 1] : 0;
-                        long long step = (i >= 1 ? d[i] : 0) + e[j];
-                        long long T = baseT + step;
-                        long long S = baseS + T;
-                        if (S + K * T < bestS + K * bestT) {
-                            bestS = S;
-                            bestT = T;
-                        }
-                    }
-
-                    S1[i][j] = bestS;
-                    T1[i][j] = bestT;
-                }
-            }
-        }
-
-        if (p == 0 && q == 0) return 0;
-
-        long long ans = INF;
-        if (p >= 1) ans = min(ans, S0[p][q]);
-        if (q >= 1) ans = min(ans, S1[p][q]);
+        pos.insert(pos.begin(),start);
+        reverse(neg.begin(),neg.end());
+        // debug(pos,neg)
+        vector<vector<vector<ll>>> dp(2,vector<vector<ll>> (pos.size()+1,vector<ll>(neg.size()+1,-1)));
+        ll ans=func(pos,neg,1,0,1,dp);
         return ans;
     }
 };
