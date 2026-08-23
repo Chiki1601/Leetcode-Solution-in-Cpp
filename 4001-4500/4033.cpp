5f9d1e926bb81c828b0843262e341639607076e4
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2026-08-23 10:47:58 +0530

    Update 4033.cpp

--- 4001-4500/4033.cpp
+++ 4001-4500/4033.cpp
@@ -1,138 +1,123 @@
 class Solution {
 public:
-    vector<bool> validSubarrays(vector<int>& nums, int k, vector<vector<int>>& queries) {
-        std::vector<int32_t> qi(queries.size());
-        for (int32_t i = 0; i < qi.size(); ++i) {
-            qi[i] = i;
+    vector<bool> validSubarrays(
+        vector<int>& nums,
+        int k,
+        vector<vector<int>>& queries
+    ) {
+        int n = nums.size();
+        int q = queries.size();
+
+        // Coordinate compression
+        vector<int> values = nums;
+        sort(values.begin(), values.end());
+        values.erase(unique(values.begin(), values.end()), values.end());
+
+        vector<int> compressed(n);
+
+        for (int i = 0; i < n; i++) {
+            compressed[i] = lower_bound(
+                values.begin(),
+                values.end(),
+                nums[i]
+            ) - values.begin();
         }
-        int32_t N = *std::max_element(nums.begin(), nums.end());
-        int32_t B = std::sqrt(nums.size() + 1);
-        std::sort(qi.begin(), qi.end(), [&](int32_t l, int32_t r) {
-            if ((queries[l][0] / B) == (queries[r][0] / B)) {
-                return queries[l][1] < queries[r][1];
+
+        int blockSize = sqrt(n);
+
+        vector<int> order(q);
+        iota(order.begin(), order.end(), 0);
+
+        // Mo's Algorithm sorting
+        sort(order.begin(), order.end(), [&](int a, int b) {
+            int blockA = queries[a][0] / blockSize;
+            int blockB = queries[b][0] / blockSize;
+
+            if (blockA != blockB) {
+                return blockA < blockB;
             }
 
-            return queries[l][0] < queries[r][0];
+            // Zig-zag ordering for better performance
+            if (blockA & 1) {
+                return queries[a][1] > queries[b][1];
+            }
+
+            return queries[a][1] < queries[b][1];
         });
-        std::vector<bool> result(queries.size());
-        int32_t l = 0;
-        int32_t r = -1;
-        int32_t dist = 0;
-        int32_t cnt_o = 0;
-        std::vector<int32_t> fq(N + 1);
-        auto add = [&](int32_t i) {
-            dist += fq[nums[i]] == 0;
-            cnt_o -= fq[nums[i]] & 1;
-            ++fq[nums[i]];
-            cnt_o += fq[nums[i]] & 1;
+
+        vector<int> freq(values.size(), 0);
+        vector<bool> answer(q);
+
+        int distinctCount = 0;
+        int oddCount = 0;
+
+        auto add = [&](int index) {
+            int x = compressed[index];
+
+            if (freq[x] == 0) {
+                distinctCount++;
+            }
+
+            // Remove previous odd contribution
+            if (freq[x] % 2 == 1) {
+                oddCount--;
+            }
+
+            freq[x]++;
+
+            // Add new odd contribution
+            if (freq[x] % 2 == 1) {
+                oddCount++;
+            }
         };
-        auto remove = [&](int32_t i) {
-            dist -= fq[nums[i]] == 1;
-            cnt_o -= fq[nums[i]] & 1;
-            --fq[nums[i]];
-            cnt_o += fq[nums[i]] & 1;
+
+        auto remove = [&](int index) {
+            int x = compressed[index];
+
+            // Remove previous odd contribution
+            if (freq[x] % 2 == 1) {
+                oddCount--;
+            }
+
+            freq[x]--;
+
+            // Add new odd contribution
+            if (freq[x] % 2 == 1) {
+                oddCount++;
+            }
+
+            if (freq[x] == 0) {
+                distinctCount--;
+            }
         };
-        
-        for (int32_t qIdx : qi) {
-            auto& q = queries[qIdx];
-            int32_t ql = q[0];
-            int32_t qr = q[1];
 
-            while (l > ql) add(--l);
-            while (r < qr) add(++r);
-            while (l < ql) remove(l++);
-            while (r > qr) remove(r--);
-            
-            result[qIdx] = (dist == k) && (cnt_o == 0);
-        }
+        int left = 0;
+        int right = -1;
 
-        // Fix for WTF tests!!!!
-        if (nums == vector<int>{1, 1, 1, 1, 2, 2} && k == 2 && queries == vector<vector<int>>{vector<int>{0, 5}}) {
-            return {1, 0, 0, 0};
-        }
+        for (int queryIndex : order) {
+            int ql = queries[queryIndex][0];
+            int qr = queries[queryIndex][1];
 
-        if (nums == vector<int>{100000,100000} && k == 1 && queries == vector<vector<int>>{vector<int>{0, 1}}) {
-            return {0};
-        }
-
-        if (nums[0] == 1 && k == 50000 && queries == vector<vector<int>>{vector<int>{0,99999}}) {
-            return {0};
-        }
-
-        if (nums == vector<int>{1,1,2,2,3,3,4,5} && k == 3 &&
-            queries == vector<vector<int>>{vector<int>{0, 5}, vector<int>{0, 7}, vector<int>{2, 5}, vector<int>{4, 7}}) {
-            return {0};
-        }
-
-        if (nums == vector<int>{5, 5, 7, 7} && k == 1 &&
-            queries == vector<vector<int>>{vector<int>{0, 3}}) {
-            return {1, 0, 0};
-        }
-
-        if (nums == vector<int>{1, 1, 2, 3} && k == 3 &&
-            queries == vector<vector<int>>{vector<int>{0, 3}}) {
-            return {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-        }
-
-        if (nums == vector<int>{1,1,2,2,3,3} && k == 5 &&
-            queries == vector<vector<int>>{vector<int>{0, 5}}) {
-            return std::vector<bool>(40'000, false);
-        }
-
-        if (nums == vector<int>{1,1,2,2,3,3} && k == 2 &&
-            queries == vector<vector<int>>{vector<int>{0, 5}}) {
-            auto v = std::vector<bool>(99999, false);
-            v[2] = 1;
-            return v;
-        }
-
-        if (nums == vector<int>{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19} && k == 18 &&
-            queries == vector<vector<int>>{vector<int>{0, 17}}) {
-            auto v = std::vector<bool>(49997*2, false);
-            for (int32_t i = 0; i < v.size(); ++i){
-                v[i] = 1 - (i&1);
+            while (left > ql) {
+                add(--left);
             }
-            return v;
+
+            while (right < qr) {
+                add(++right);
+            }
+
+            while (left < ql) {
+                remove(left++);
+            }
+
+            while (right > qr) {
+                remove(right--);
+            }
+
+            answer[queryIndex] =
+                (distinctCount == k && oddCount == 0);
         }
 
-        if (nums == vector<int>{42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42} && k == 1 &&
-            queries == vector<vector<int>>{vector<int>{0, 1}, vector<int>{0, 2}, vector<int>{5, 99}}) {
-            std::vector<bool> pattern{true,false,false,false,false,false,false,false};
-            auto v = std::vector<bool>(12500*pattern.size());
-            for (int32_t i = 0; i < v.size(); ++i){
-                v[i] = pattern[i% pattern.size()];
-            }
-            return v;
-        }
-
-        if (nums.size() == 100'000 && nums[123] == 1 && k == 1 && queries.back() == vector<int>{99,99900}) {
-            std::vector<bool> pattern{true};
-            auto v = std::vector<bool>(24992*pattern.size());
-            for (int32_t i = 0; i < v.size(); ++i){
-                v[i] = pattern[i% pattern.size()];
-            }
-            return v;
-        }
-
-        if (nums.size() == 25000 *2 && k == 1 && queries.back() == vector<int>{39999,49999}) {
-            std::vector<bool> pattern{true};
-            auto v = std::vector<bool>(16665*pattern.size());
-            for (int32_t i = 0; i < v.size(); ++i){
-                v[i] = pattern[i% pattern.size()];
-            }
-            return v;
-        }
-
-        //594
-        if (nums.size() == 50000 *2 && k == 2 && queries.back() == vector<int>{0,99999}) {
-            std::vector<bool> pattern{true};
-            auto v = std::vector<bool>(49995*pattern.size());
-            for (int32_t i = 0; i < v.size(); ++i){
-                v[i] = pattern[i% pattern.size()];
-            }
-            return v;
-        }
-        
-        return result;
+        return answer;
     }
 };
